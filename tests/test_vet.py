1b55ccb692fb8305d6e73a1566e1927e9f22b6be
Timothy Saudners <timsaunders88@gmail.com>
2020-08-07 13:53:34 +0100

    created project basic structure, added vet model and test

---
+++ tests/test_vet.py
@@ -0,0 +1,10 @@
+import unittest
+from models.vet import Vet
+
+class TestVet(unittest.TestCase):
+    def setUp(self):
+        self.vet_1 = Vet("John", "Smith")
+    
+    def test_initialisation(self):
+        self.assertEqual("John", self.vet_1.first_name)
+        self.assertEqual("Smith", self.vet_1.last_name)
