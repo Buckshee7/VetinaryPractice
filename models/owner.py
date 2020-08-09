75f9321256b34c3249681468100c5846a523ae24
Timothy Saudners <timsaunders88@gmail.com>
2020-08-09 15:37:15 +0100

    added models/owner.py and Owner class

---
+++ models/owner.py
@@ -0,0 +1,6 @@
+class Owner():
+    def __init__(self, title, first_name, last_name, registered):
+        self.title = title
+        self.first_name = first_name
+        self.last_name = last_name
+        self.registered = registered
