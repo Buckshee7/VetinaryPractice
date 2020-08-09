85cc14664402e75a0d0774d4708c7fe08ea865b0
Timothy Saudners <timsaunders88@gmail.com>
2020-08-09 15:33:54 +0100

    added owners table to db/vet.sql

--- db/vet.sql
+++ db/vet.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS animals;
+DROP TABLE IF EXISTS owners;
 DROP TABLE IF EXISTS vets;
 
 CREATE TABLE vets (
@@ -8,14 +9,21 @@ CREATE TABLE vets (
     img_url VARCHAR(255)
 );
 
+CREATE TABLE owners (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255),
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    registered BOOLEAN
+);
+
 CREATE TABLE animals (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255),
     img_url VARCHAR(255),
     dob VARCHAR(255),
     animal_type VARCHAR(255),
-    owner_name VARCHAR(255),
-    owner_phone VARCHAR(255),
+    owner_id INT REFERENCES owners(id),
     treatment_notes TEXT,
     vet_id INT REFERENCES vets(id) ON DELETE SET NULL 
 );
