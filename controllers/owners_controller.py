c3b4866bbe8681b14039d8830794900a2dadeb04
Timothy Saudners <timsaunders88@gmail.com>
2020-08-09 23:00:41 +0100

    added all other routes to owners_controller and added owners/edit.html with basic content

--- controllers/owners_controller.py
+++ controllers/owners_controller.py
@@ -26,12 +26,27 @@ def get_info_for_new():
 
 @owners_blueprint.route('/owners', methods=['POST'])
 def create_new_owner():
-    pass
+    title = request.form['title']
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    phone = request.form['phone']
+    registered = True if request.form['registered'] == "True" else False
+    new_owner = Owner(title, first_name, last_name, phone, registered)
+    owner_repository.save(new_owner)
+    return redirect('/owners')
 
 @owners_blueprint.route('/owners/<id>/edit')
 def get_info_for_update(id):
-    pass
+    owner = owner_repository.select(id)
+    return render_template('/owners/edit.html', owner=owner, title=f'Update {owner.title} {owner.last_name}')
 
 @owners_blueprint.route('/owners/<id>', methods=['POST'])
 def update_owner(id):
-    pass
+    title = request.form['title']
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    phone = request.form['phone']
+    registered = True if request.form['registered'] == "True" else False
+    updated_owner = Owner(title, first_name, last_name, phone, registered, id)
+    owner_repository.update(updated_owner)
+    return redirect('/owners')
