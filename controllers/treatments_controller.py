065e0feb4fa0edf5eb75fe8253f6068c5a5f55ab
Timothy Saudners <timsaunders88@gmail.com>
2020-08-10 19:34:55 +0100

    added routes for new treatments and updating treatments and adjusted show.html files to accomodate

--- controllers/treatments_controller.py
+++ controllers/treatments_controller.py
@@ -13,23 +13,52 @@ def delete_from_vet(id, source):
     treatment = treatment_repository.select(id)
     if source == "vet":
         source_id = vet_repository.select(treatment.vet.id).id
-    else:
+    elif source == "animal":
         source_id = animal_repository.select(treatment.animal.id).id
     treatment_repository.delete(id)
     return redirect(f'/{source}s/{source_id}')
 
-@treatments_blueprint.route('/treatments/new/<source>')
-def get_info_for_new(source):
-    return render_template('/treatments/new.html', source=source, title='New Treatment')
+#<animal_id> is the source animal the treatment is being applied to - used for redirecting after addition
+@treatments_blueprint.route('/treatments/new/<animal_id>')
+def get_info_for_new(animal_id):
+    animal = animal_repository.select(animal_id)
+    vet = animal.vet
+    return render_template('/treatments/new.html', animal=animal, vet=vet, title='New Treatment')
 
 @treatments_blueprint.route('/treatments', methods=['POST'])
-def create_new_vet():
-    pass
+def create_new_treatment():
+    animal_id = request.form['animal_id']
+    animal = animal_repository.select(animal_id)
+    vet_id = request.form['vet_id']
+    vet = vet_repository.select(vet_id)
+    date = request.form['date']
+    details = request.form['details']
+    new_treatment = Treatment(animal, vet, details, date)
+    treatment_repository.save(new_treatment)
+    return redirect(f'/animals/{animal_id}')
 
-@treatments_blueprint.route('/treatments/<id>/edit')
-def get_info_for_update(id):
-    pass
+@treatments_blueprint.route('/treatments/<id>/edit/<source>')
+def get_info_for_update(id, source):
+    treatment = treatment_repository.select(id)
+    vet = vet_repository.select(treatment.vet.id)
+    animal = animal_repository.select(treatment.animal.id)
+    return render_template('treatments/edit.html', treatment=treatment, source=source, vet=vet, animal=animal, title='Update Treatment')
 
 @treatments_blueprint.route('/treatments/<id>', methods=['POST'])
 def update_treatment(id):
-    pass
+    animal_id = request.form['animal_id']
+    animal = animal_repository.select(animal_id)
+    vet_id = request.form['vet_id']
+    vet = vet_repository.select(vet_id)
+    date = request.form['date']
+    details = request.form['details']
+
+    source = request.form['source']
+    if source == "vet":
+        source_id = vet.id
+    elif source == "animal":
+        source_id = animal.id
+
+    updated_treatment = Treatment(animal, vet, details, date, id)
+    treatment_repository.update(updated_treatment)
+    return redirect(f'/{source}s/{source_id}')
