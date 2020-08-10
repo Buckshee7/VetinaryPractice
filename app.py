4ef852d3850debabd2dc1e1c71d1a8d54e737558
Timothy Saudners <timsaunders88@gmail.com>
2020-08-10 20:26:15 +0100

    added information to templates/index.html, app.py updated to send information with template_render

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 from flask import Flask, render_template
+import datetime
+import repositories.treatment_repository as treatment_repository
+import repositories.owner_repository as owner_repository
+import repositories.animal_repository as animal_repository
+import repositories.vet_repository as vet_repository
 
 app = Flask(__name__)
 
@@ -16,7 +21,14 @@ app.register_blueprint(treatments_blueprint)
 
 @app.route('/')
 def home():
-    return render_template('index.html', title='Home')
+    owners = owner_repository.select_all()
+    animals = animal_repository.select_all()
+    vets = vet_repository.select_all()
+    counts = {'owners':len(owners), 'animals':len(animals), 'vets':len(vets)}
+    date_today = datetime.datetime.now().date()
+    treatments = treatment_repository.select_all()
+    treatments_today = [treatment for treatment in treatments if treatment.date == date_today]
+    return render_template('index.html', treatments_today=treatments_today, counts=counts, title='Home')
 
 if __name__ == '__main__':
     app.run(debug=True)
