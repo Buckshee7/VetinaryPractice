40b42c7f8b6e832113f0f39e64f4e93aed3a74c5
Timothy Saudners <timsaunders88@gmail.com>
2020-08-10 16:20:08 +0100

    added routes for /treatments/<id>/delete and modified vet and animal show.htmls to incorporate

--- controllers/treatments_controller.py
+++ controllers/treatments_controller.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, render_template, request, redirect
 import repositories.treatment_repository as treatment_repository
+import repositories.vet_repository as vet_repository
+import repositories.animal_repository as animal_repository
 import datetime
 from models.treatment import Treatment
 
@@ -19,13 +21,31 @@ def index():
 #     treatment = treatment_repository.select(id)
 #     return render_template('treatments.show.html', treatment=treatment, title='Treatment Details')
 
-@treatments_blueprint.route('/treatments/<id>/delete')
+#route for delete FROM main treatments page
+@treatments_blueprint.route('/treatments/<id>/delete/')
 def delete(id):
-    pass
+    treatment_repository.delete(id)
+    return redirect('/treatments')
+
+#route for delete FROM vet page
+@treatments_blueprint.route('/treatments/<id>/delete/vet')
+def delete_from_vet(id):
+    treatment = treatment_repository.select(id)
+    vet = vet_repository.select(treatment.vet.id)
+    treatment_repository.delete(id)
+    return redirect(f'/vets/{vet.id}')
+
+#route for delete FROM animal page
+@treatments_blueprint.route('/treatments/<id>/delete/animal')
+def delete_from_animal(id):
+    treatment = treatment_repository.select(id)
+    animal = animal_repository.select(treatment.animal.id)
+    treatment_repository.delete(id)
+    return redirect(f'/animals/{animal.id}')
 
 @treatments_blueprint.route('/treatments/new')
 def get_info_for_new():
-    pass
+    return render_template('/treatments/new.html', title='New Treatment')
 
 @treatments_blueprint.route('/treatments', methods=['POST'])
 def create_new_vet():
